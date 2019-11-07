4eae7843e4e27577379d67a67ced942be156301d
Stephanie Hooker <stephanieh799@gmail.com>
2019-11-07 13:10:15 -0700

    first practice assignment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
+  state = {
+    username: 'Stephanie'
+  }
+
+  usernameChangedHandler = (event) => {
+    this.setState({ username: event.target.value });
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, Im a React App</h1>
+        <UserInput
+          changed={this.usernameChangedHandler}
+          currentName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName="Max" />
       </div>
     );
   }

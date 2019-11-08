1e002f93d6947dbb09a06f412a52739cb0c8974b
Stephanie Hooker <stephanieh799@gmail.com>
2019-11-07 20:42:38 -0700

    list practice assignment

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
   state = {
-    username: 'Stephanie'
+    username: '',
+    userInput: ''
+  }
+
+  inputChangedHandler = (event) => {
+    this.setState({ userInput: event.target.value });
+
+  }
+
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({ userInput: updatedText });
   }
 
   usernameChangedHandler = (event) => {
     this.setState({ username: event.target.value });
   }
   render() {
+    let charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char
+        character={ch}
+        key={index}
+        clicked={() => this.deleteCharHandler(index)} />;
+    });
     return (
       <div className="App">
         <h1>Hi, Im a React App</h1>
@@ -21,7 +42,15 @@ class App extends Component {
         <UserOutput userName={this.state.username} />
         <UserOutput userName={this.state.username} />
         <UserOutput userName="Max" />
+        <hr />
+        <input type="text"
+          onChange={this.inputChangedHandler}
+          value={this.state.userInput} />
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
+        {charList}
       </div>
+
     );
   }
 }
